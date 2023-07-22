495c3bb9b25364e6389f8e7bdf2ca44dd0404b03
Pavitar Rana <ranapavitar14@gmail.com>
2023-07-23 00:38:36 +0530

    created todo app

---
+++ yt-todo/components/GoalItem.js
@@ -0,0 +1,33 @@
+import { StyleSheet, View, Text, Pressable } from "react-native";
+
+function GoalItem(props) {
+  return (
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#dddddd" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+  goalItem: {
+    height: 40,
+    justifyContent: "center",
+    margin: 5,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  pressedItem: {
+    opacity: 0.5,
+  },
+  goalText: {
+    color: "white",
+    padding: 8,
+  },
+});
